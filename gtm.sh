12241ee3982413da392b4c1036706f7e1fefcf72
Viktor Szépe <viktor@szepe.net>
2023-09-16 17:07:16 +0000

    Improve shell script

--- gtm.sh
+++ gtm.sh
@@ -4,26 +4,26 @@
 #
 # DEPENDS       :apt-get install wget jq
 
-MEASUREMENT_ID="$1"
+TAG_ID="$1"
 
 set -e
 
-grep -E '^[A-Z]+-[0-9A-Z]{8,}$' <<<"${MEASUREMENT_ID}"
+grep -E '^[A-Z]+-[0-9A-Z]{7,}$' <<<"${TAG_ID}"
 
-if [ "${MEASUREMENT_ID:0:4}" == "GTM-"; then
-    wget -O "${MEASUREMENT_ID}.js" "https://www.googletagmanager.com/gtm.js?id=${MEASUREMENT_ID}"
+if [ "${TAG_ID:0:4}" == "GTM-"; then
+    wget -O "${TAG_ID}.js" "https://www.googletagmanager.com/gtm.js?id=${TAG_ID}"
 else
-    wget -O "${MEASUREMENT_ID}.js" "https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}"
+    wget -O "${TAG_ID}.js" "https://www.googletagmanager.com/gtag/js?id=${TAG_ID}"
 fi
 
 # Extract JSON object
-cat "${MEASUREMENT_ID}.js" \
+cat "${TAG_ID}.js" \
     | sed -e '/^var data = {$/,/^};$/!d' \
     | sed -e '1s/^var data = //;$s/;$//' \
-    >"${MEASUREMENT_ID}.json"
+    >"${TAG_ID}.json"
 
 # Display JavaScript events on elements with a certain HTML class
-#jq -r '."resource"."predicates"[] | select(."function" == "_cn") | ."arg1"' "${MEASUREMENT_ID}.json"
+#jq -r '."resource"."predicates"[] | select(."function" == "_cn") | ."arg1"' "${TAG_ID}.json"
 
 # Display all tags
-jq -r '."resource"."tags"[]' "${MEASUREMENT_ID}.json"
+jq -r '."resource"."tags"[]' "${TAG_ID}.json"
